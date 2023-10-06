b669cf4251948fd389650651a0f1321f4a52e5fd
xdiffan <irfanda4567@gmail.com>
2023-10-06 11:25:51 +0700

    make profile layout

--- app/src/main/java/com/challenge/foodappchallenge3/presentation/profile/FragmentProfile.kt
+++ app/src/main/java/com/challenge/foodappchallenge3/presentation/profile/FragmentProfile.kt
@@ -5,20 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.challenge.foodappchallenge3.R
+import com.challenge.foodappchallenge3.databinding.FragmentProfileBinding
 
 
 class FragmentProfile : Fragment() {
+    private lateinit var binding: FragmentProfileBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(
-            R.layout.fragment_profile,
+    ): View {
+        binding = FragmentProfileBinding.inflate(
+            inflater,
             container,
             false
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(
+            view,
+            savedInstanceState
         )
     }
 
